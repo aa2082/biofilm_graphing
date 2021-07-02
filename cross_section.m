5ab10ef0edd87cdb6a96ed85e508f43f31241f9d
aa2082 <aa2082@cam.ac.uk>
2021-07-02 15:35:10 +0100

    made length_plot function work for both full plot and cross sections

--- cross_section.m
+++ cross_section.m
@@ -1,23 +1,20 @@
-function cross_section(cut1,dir,color_label)
+function scatter_plot(cut_dir,cut1,color_label)
     global x y z c i_cells thickness dot_size plot_range;
     figure
     scatter3(x,y,z,dot_size,c, 'filled');
     cut2 = cut1+thickness;
-    range = plot_range;
-    if dir == 'X'
+    range = plot_range;   
+    if cut_dir == 'X'
         range(1) = cut1;
         range(2) = cut2;
-        %axis(range)
         view([-90 0])
-    elseif dir == 'Y'
+    elseif cut_dir == 'Y'
         range(3) = cut1;
         range(4) = cut2;
-        %axis(range)
         view([0 0])
-    elseif dir == 'Z'
+    elseif cut_dir == 'Z'
         range(5) = cut1;
         range(6) = cut2;
-        %axis(range)
         view([0 -90])
     else
         error("axis incorrect, can only be X, Y or Z");
@@ -31,5 +28,5 @@ function cross_section(cut1,dir,color_label)
     h1.Label.String = color_label;
     colormap(parula(10))
     caxis([0, ceil(max(c))]);
-    title(['time = ' num2str(i_cells*.1) 'h - ' dir ' cross section']);
+    title(['time = ' num2str(i_cells*.1) 'h - ' cut_dir ' cross section']);
 end
